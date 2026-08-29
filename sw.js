3f621aea168d79956982960f3f936172f5c9cf25
jianyu <jianyu@local>
2026-08-29 20:49:22 +0800

    fix: v71 buildItemIndex getExcludes 支持嵌套括号，阻止被除外的 L2 通类项扩散到 L3 子类

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // GB2762 Cache Buster Service Worker
 // 拦截所有 fetch 请求,绕过浏览器 disk cache,强制从服务器拉最新
 
-const CACHE_BUST = 'v70-other-oil-products-inorganic-arsenic-2026-08-29-20:35';
+const CACHE_BUST = 'v71-nested-paren-exclude-diffusion-2026-08-29-20:42';
 
 self.addEventListener('install', (event) => {
   // 立即激活,不等旧 SW 关闭
